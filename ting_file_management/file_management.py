b7c6195b6619eb1cb6cc39a84b997c4532e818de
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-12-01 19:00:50 -0300

    Req 02 ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr) 
+
+    try:
+        with open(path_file, 'r') as patch:
+            return patch.read().split("\n")
+    except FileNotFoundError:    
+        # sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
