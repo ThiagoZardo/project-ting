4b775fdaa2fc552370f47093005a4c4b448916b8
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-12-01 20:50:58 -0300

    Req 03 ok

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": (file),
+    }
+
+    print(f'{data}', file=sys.stdout)
+    instance.enqueue(data)
+    return data
 
 
 def remove(instance):
