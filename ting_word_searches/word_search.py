456c2f2f4c6cc0e1bc9d02397cf32d7c7710715b
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-12-01 23:53:06 -0300

    Iniciando req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,10 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_insensitive = word.lower()
+    line_of_file = []
+    for i in range(len(instance)):
+        if instance.search(i) == word_insensitive:
+            line_of_file.append(instance['linhas_do_arquivo'])
+    return line_of_file
 
 
 def search_by_word(word, instance):
