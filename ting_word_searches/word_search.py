97e7dc130a4306167e7f98b981e57007961ca90c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-12-13 23:16:43 -0300

    Ultimas modificações

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,6 +4,7 @@ def exists_word(word, instance):
     for i in range(len(instance)):
         if instance.search(i) == word_insensitive:
             line_of_file.append(instance['linhas_do_arquivo'])
+    print('texto', line_of_file)
     return line_of_file
 
 
